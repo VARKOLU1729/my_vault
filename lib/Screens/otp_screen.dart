de9a7763d662d623c93be74133b9832774a06e1a
VARKOLU1729 <115711677+VARKOLU1729@users.noreply.github.com>
2025-01-13 21:43:44 +0530

    Update otp_screen.dart

--- lib/Screens/otp_screen.dart
+++ lib/Screens/otp_screen.dart
@@ -1,23 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:my_vault/Helpers/send_message.dart';
 
 class OtpScreen extends StatefulWidget {
-
   final String verificationId;
-  
-  OtpScreen({super.key, required this.verificationId});
+
+  OtpScreen({Key? key, required this.verificationId}) : super(key: key);
 
   @override
   State<OtpScreen> createState() => _OtpScreenState();
 }
 
 class _OtpScreenState extends State<OtpScreen> {
-
-  final auth = FirebaseAuth.instance;
-  String otp = "";
-
+  final FirebaseAuth auth = FirebaseAuth.instance;
   final List<TextEditingController> _controllers =
-  List.generate(6, (_) => TextEditingController());
+      List.generate(6, (_) => TextEditingController());
 
   @override
   void dispose() {
@@ -27,24 +25,18 @@ class _OtpScreenState extends State<OtpScreen> {
     super.dispose();
   }
 
-  String _getOTPValue() {
-    return _controllers.map((controller) => controller.text).join();
-  }
+  final firestore = FirebaseFirestore.instance;
 
-  @override
-  void initState()
-  {
-    super.initState();
-  }
+  String getOtpValue() =>
+      _controllers.map((controller) => controller.text).join();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        mainAxisAlignment : MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
         mainAxisSize: MainAxisSize.min,
         children: [
-
           const Text(
             "Enter Your 6 digit OTP",
             style: TextStyle(
@@ -53,73 +45,65 @@ class _OtpScreenState extends State<OtpScreen> {
               fontSize: 25,
             ),
           ),
-
-          buildOtpFields(
-              context: context,
-              otp: otp
-          ),
-
+          buildOtpFields(context),
           TextButton(
-              onPressed: () async{
-                // AuthCredential authCredential = PhoneAuthProvider.credential(
-                //     verificationId: widget.verificationId,
-                //     smsCode: otp
-                // );
-                //
-                // try{
-                //   await auth.signInWithCredential(authCredential);
-                // }
-                // catch(e)
-                // {
-                //   print(e.toString());
-                // }
+            onPressed: () async {
+              AuthCredential authCredential = PhoneAuthProvider.credential(
+                verificationId: widget.verificationId,
+                smsCode: getOtpValue(),
+              );
 
-                print("++++++++++++++++++++++++ $otp");
-                
-              },
-              child: const Text(
-                "Verify",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
-                ),
-              )
-          )
+              try {
+                await auth.signInWithCredential(authCredential);
 
+                SendMessage(context: context, message: "Auth Success");
+                // Navigate to the next screen upon successful verification
+              } catch (e) {
+                print(e.toString());
+                SendMessage(context: context, message: "Auth Failed");
+                // Display error message to the user
+              }
+            },
+            child: const Text(
+              "Verify",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
-}
 
-
-Widget buildOtpFields({required BuildContext context, required String otp})
-{
-  return Row(
-    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-    children: List.generate(6, (index) {
-      return SizedBox(
-        width: 50,
-        height: 50,
-        child: TextField(
-          onChanged: (value) {
-            if (value.isNotEmpty && index < 5) {
-
-              FocusScope.of(context).nextFocus();
-            }
-          },
-          textAlign: TextAlign.center,
-          maxLength: 1,
-          keyboardType: TextInputType.number,
-          decoration: InputDecoration(
-            counterText: "",
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(5),
+  Widget buildOtpFields(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: List.generate(6, (index) {
+        return SizedBox(
+          width: 50,
+          height: 50,
+          child: TextField(
+            controller: _controllers[index],
+            onChanged: (value) {
+              if (value.isNotEmpty && index < 5) {
+                FocusScope.of(context).nextFocus();
+              }
+            },
+            textAlign: TextAlign.center,
+            maxLength: 1,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              counterText: "",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
             ),
           ),
-        ),
-      );
-    }),
-  );
+        );
+      }),
+    );
+  }
 }
