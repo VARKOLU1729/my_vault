3b55673abd80bb2b72ba68e95d5d4ea88ebb5663
VARKOLU1729 <115711677+VARKOLU1729@users.noreply.github.com>
2025-01-13 21:44:49 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,37 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
 import 'package:my_vault/Screens/login_screen.dart';
-
+import 'package:my_vault/Screens/MPinScreen.dart';
 import 'firebase_options.dart';
 
-void main() async
-{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(
-      MaterialApp(
-        home: StreamBuilder(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, snapshot)
-            {
-              Widget widget = LoginScreen();
-              final ConnectionState connectionState = snapshot.connectionState;
-              if(connectionState == ConnectionState.waiting)
-                {
-                  widget =  const Center(child: CircularProgressIndicator());
-                }
-              else if(connectionState == ConnectionState.done)
-                {
-                  // if(snapshot.hasData) widget = MPinScreen();
-                }
-              return widget;
-            }
-        ),
-      )
-  );
-}
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
+  runApp(MaterialApp(
+    home: StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        print(snapshot.data);
+
+        Widget widget = LoginScreen();
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          print("Connection Waiting");
+          widget = Center(
+            child: CircularProgressIndicator(
+              color: Colors.blue,
+            ),
+          );
+        } else if (snapshot.connectionState == ConnectionState.active) {
+          print("Connection done");
+          if (snapshot.hasData) {
+            widget = MPinScreen();
+          }
+        }
+        return widget;
+      },
+    ),
+  ));
+}
